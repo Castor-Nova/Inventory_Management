9f63af16ca96097aa4044dc88e248ebf5ef7c3e5
James_from_windows <novacastor8899@gmail.com>
2025-03-10 14:19:45 +0530

    added the powershell support

--- main.c
+++ main.c
@@ -104,11 +104,84 @@ void deleteProduct(int id) {
     }
 }
 
+void logAction(const char *action) {
+    char command[256];
+    snprintf(command, sizeof(command), "powershell.exe -Command \"& .\\logAction.ps1 '%s'\"", action);
+    system(command); // Execute the PowerShell script
+}
+
 int main() {
-    displayProducts();
+    int choice;
+    while (1) {
+        // Display menu options
+        printf("\nOptions:\n");
+        printf("1: Display Products\n");
+        printf("2: Purchase Products\n");
+        printf("3: Add Products\n");
+        printf("4: Delete Products\n");
+        printf("0: Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
 
-    purchaseProduct(2, 10);
-    purchaseProduct(3, 5);
+        if (choice == 0) {
+            printf("Exiting the program.\n");
+            break;
+        }
 
-    displayProducts();
+        switch (choice) {
+            case 1: // Display products
+                displayProducts();
+                logAction("Displayed all products.");
+                break;
+
+            case 2: { // Purchase products
+                int id, quantity;
+                printf("Enter Product ID to purchase: ");
+                scanf("%d", &id);
+                printf("Enter quantity to purchase: ");
+                scanf("%d", &quantity);
+                purchaseProduct(id, quantity);
+                
+                char log[100];
+                snprintf(log, sizeof(log), "Purchased %d of product ID %d.", quantity, id);
+                logAction(log);
+                break;
+            }
+
+            case 3: { // Add product
+                Product newProduct;
+                printf("Enter Product ID: ");
+                scanf("%d", &newProduct.id);
+                printf("Enter Product Name: ");
+                scanf("%s", newProduct.name);
+                printf("Enter Product Price: ");
+                scanf("%f", &newProduct.price);
+                printf("Enter Product Quantity: ");
+                scanf("%d", &newProduct.quantity);
+                addProduct(newProduct);
+                
+                char log[100];
+                snprintf(log, sizeof(log), "Added product ID %d (%s).", newProduct.id, newProduct.name);
+                logAction(log);
+                break;
+            }
+
+            case 4: { // Delete product
+                int id;
+                printf("Enter Product ID to delete: ");
+                scanf("%d", &id);
+                deleteProduct(id);
+                
+                char log[100];
+                snprintf(log, sizeof(log), "Deleted product ID %d.", id);
+                logAction(log);
+                break;
+            }
+
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+    }
+
+    return 0;
 }
